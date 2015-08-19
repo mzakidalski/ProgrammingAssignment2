102359f0c44b5e6b9b65e2173aed32fc5c8e59e8
Marcin Zakidalski <Marcin_Zakidalski@epam.com>
2015-08-19 12:17:48 +0200

    Final version of the code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,49 @@
 
+
+##
+## Function calls method with given methodName from
+## object.
+## Precodition: such method was added with addMethod function
+##              or in similar manner
+## Params:
+##        object - where the method should be added
+##        methodName - name of method to be called
+##        ... - variable-length list of arguments
+##              passed to the method
+## Returns: 
+##        - result of method call
+##
+##
+
 callMethod <- function (object, methodName, ...) {
   method <- attr(object, methodName)
   method(...)
 }
+
+##
+## Function adds method to given object
+##
+## Params: 
+##        object - where the method should be added
+##        methodName - name of the method
+##        methodBody - method body
+##
+##
 
 addMethod  <- function(object, methodName, methodBody) {
   attr(object, methodName) <- methodBody
   object
 }
 
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##
+## Function creates matrix caching its inverse matrix
+## Inverse matrix is stored in inverseX global variable.
+## Function also adds appropriate methods to the matrix
+## Params: 
+##      x - source matrix
+## Returns:
+##      matrix (based on x) caching its reverse matrix 
+##
 
 makeCacheMatrix <- function(x = matrix()) {
   inverseX <- NULL
@@ -38,7 +69,16 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+##
+## Method returns cached inverse matrix. If
+## no cached value is available, then computation
+## are performed and their result is stored in cache.
+## Params:
+##    x- matrix to be reversed
+## Returns:
+##    inverse matrix of x
+##
+##
 
 cacheSolve <- function(x, ...) {
   inverse <- callMethod(x,"getInverse")
--- testExecutor.R
+++ testExecutor.R
@@ -1,18 +1,24 @@
 source("cachematrix.R")
 
+message("Simple test case showing the code works correctly!")
+message("Computing the inverse matrix of two given matrices alternately!")
 
-## Test one - unity matrix
 initialMatrixOne <- matrix (c(1,0,0,1),2,2)
-class(initialMatrixOne)
 cacheMatrixOne <- makeCacheMatrix(initialMatrixOne)
-cacheSolve(cacheMatrixOne)
-cacheSolve(cacheMatrixOne)
+message("\n\nFirst matrix")
+print(cacheMatrixOne)
 
-## Test two - other matrix
 initialMatrixTwo <- matrix (c(1,2,3,0, 1,4, 5,6,0),3,3)
-class(initialMatrixTwo)
 cacheMatrixTwo <- makeCacheMatrix(initialMatrixTwo)
-cacheSolve(cacheMatrixTwo)
-cacheSolve(cacheMatrixTwo)
+message("\n\nSecond matrix")
+print(cacheMatrixTwo)
+
+for (i in 1:2)
+{
+  print(cacheSolve(cacheMatrixOne))
+  print(cacheSolve(cacheMatrixTwo))
+  
+}
+
 
 
